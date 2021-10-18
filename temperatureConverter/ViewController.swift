36717e31f9f85b8ceeb975e7e9ae5864ab8ed06d
michael21910 <410821305@gms.ndhu.edu.tw>
2021-10-18 16:26:07 +0800

    change UI, add image and change colors; more clear code

--- temperatureConverter/ViewController.swift
+++ temperatureConverter/ViewController.swift
@@ -9,54 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // font color
+    let myFontColor = UIColor(red: 105/255, green: 70/255, blue: 53/255, alpha: 1.0)
+    let myBackgroundColor = UIColor(red: 242/255, green: 240/255, blue: 221/255, alpha: 1.0)
+    
     @IBOutlet weak var inputTemp: UITextField!
     @IBOutlet weak var outputTemp: UILabel!
     
     @IBAction func tempConvert(sender: UIButton) {
         // if the text field is not empty
         if(inputTemp.text != "") {
-            // check whether the number is valid
-            var isValid:Bool = true
-            var digit = 0
-            for ch in inputTemp.text! {
-                // negative number
-                if(digit == 0 && ch == "-") {
-                    continue
+            if let temp = Double(inputTemp.text!) {
+                let num = String(format: "%.2f", (temp - 32) * 5 / 9)
+                outputTemp.text = num + "ºC"
+                if((Double(num)!) <= 0) {
+                    outputTemp.textColor = UIColor.white
+                    outputTemp.backgroundColor = UIColor.blue
                 }
-                // invalid number situation
-                if(!("0" <= ch && ch <= "9")) {
-                    isValid = false
-                    break
+                else if((Double(num)!) >= 100) {
+                    outputTemp.textColor = UIColor.white
+                    outputTemp.backgroundColor = UIColor.red
                 }
-                digit += 1
+                else {
+                    outputTemp.textColor = UIColor.white
+                    outputTemp.backgroundColor = UIColor.orange
+                }
             }
-            // the number that the user input is a valid number
-            if(isValid == true) {
-                if let temp = Double(inputTemp.text!) {
-                    let num = String(format: "%.2f", (temp - 32) * 5 / 9)
-                    outputTemp.text = num + "ºC"
-                }
-                let alertController = UIAlertController(title: "Correct input!", message: "Successfully converted", preferredStyle: .alert)
+            // if the text field not empty
+            else {
+                let alertController = UIAlertController(title: "Something went wrong!", message: "This is not a valid number!", preferredStyle: .alert)
                 alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
                 present(alertController, animated: true, completion: nil)
+        
+                // the style of the output area
+                outputTemp.textColor = myFontColor
+                outputTemp.backgroundColor = myBackgroundColor
+                outputTemp.text = "error"
             }
-            // the number that the user input is not a valid number
-            else {
-                let alertController = UIAlertController(title: "Something went wrong!", message: "This is not a number, please try again", preferredStyle: .alert)
-                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                present(alertController, animated: true, completion: nil)            }
         }
-        // if the text field not empty, show error message
+        // if the text field is empty, show the error message
         else {
             let alertController = UIAlertController(title: "Something went wrong!", message: "Please input the temperature in the text field", preferredStyle: .alert)
             alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
             present(alertController, animated: true, completion: nil)
+            
+            // the style of the output area
+            outputTemp.textColor = myFontColor
+            outputTemp.backgroundColor = myBackgroundColor
+            outputTemp.text = "error"
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // initialize the placeholder of the output area
+        outputTemp.text = "output area"
+        outputTemp.textColor = myFontColor
+        outputTemp.backgroundColor = UIColor(red: 242/255, green: 240/255, blue: 221/255, alpha: 1.0)
     }
 
 }
-
